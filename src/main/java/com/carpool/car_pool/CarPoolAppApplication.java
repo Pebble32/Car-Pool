72790518f8855b444d4fab07ebc87e3f96362fe0
Adam Postek <bjkk32@gmail.com>
2024-09-05 18:19:03 +0000

    Batman

---
+++ src/main/java/com/carpool/car_pool/CarPoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.carpool.car_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPoolAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carpool/car_pool/CarPoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpool.car_pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
