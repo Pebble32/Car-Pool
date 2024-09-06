20a40d59fc0ac504a66526f35bf668d76b64d55b
MatioS <dkm2@hi.is>
2024-09-06 18:06:47 +0000

    First comment

--- src/main/java/com/carpool/car_pool/CarPoolAppApplication.java
+++ src/main/java/com/carpool/car_pool/CarPoolAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarPoolAppApplication {
-
+// dkm2@hi.is
 	public static void main(String[] args) {
 		SpringApplication.run(CarPoolAppApplication.class, args);
 	}
