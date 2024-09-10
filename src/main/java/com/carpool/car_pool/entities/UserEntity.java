d5bc10a9876d6d7017b098d7ccbcb95b12b29ac9
adamp <bjkk32@gmail.com>
2024-09-10 15:12:34 +0000

    Typo in application

---
+++ src/main/java/com/carpool/car_pool/entities/UserEntity.java
@@ -0,0 +1,20 @@
+package com.carpool.car_pool.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
