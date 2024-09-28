da3789858dc13c8b72a49539dcf4f61fcdef3d06
Adam Postek <bjkk32@gmail.com>
2024-09-28 18:44:33 +0000

    Small changes

--- src/main/java/com/carpool/car_pool/services/RideRequestService.java
+++ src/main/java/com/carpool/car_pool/services/RideRequestService.java
@@ -3,9 +3,13 @@ package com.carpool.car_pool.services;
 import com.carpool.car_pool.controllers.dtos.RideRequestRequest;
 import com.carpool.car_pool.repositories.RideOfferRepository;
 import com.carpool.car_pool.repositories.UserRepository;
+import com.carpool.car_pool.repositories.entities.RideRequestsEntity;
+import com.carpool.car_pool.services.converters.RideRequestConverter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.carpool.car_pool.repositories.entities.RequestStatus.PENDING;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +18,7 @@ public class RideRequestService {
 
     private final RideOfferRepository rideOfferRepository;
     private final UserRepository userRepository;
+    private final RideRequestConverter rideRequestConverter;
 
     public Long createRideOffer(@Valid RideRequestRequest request, String userEmail) {
         var rideOffer = rideOfferRepository.findById(request.getRideOfferId())
@@ -25,6 +30,12 @@ public class RideRequestService {
                 .orElseThrow(() -> new RuntimeException("User Not Found"));
 
         if (rideOffer.getAvailableSeats() <= 0) throw new RuntimeException("Ride Offer Not Available");
+
+        var rideRequestEntity = RideRequestsEntity.builder()
+                .rideOffer(rideOffer)
+                .requester(rideRequster)
+                .requestStatus(PENDING)
+                .build();
 
 
     }
