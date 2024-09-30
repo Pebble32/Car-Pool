3340500bb182a70298bf8f76626733b7e0318554
adamp <bjkk32@gmail.com>
2024-09-30 14:41:51 +0000

    Response DTO added

--- src/main/java/com/carpool/car_pool/controllers/dtos/RideRequestResponse.java
+++ src/main/java/com/carpool/car_pool/controllers/dtos/RideRequestResponse.java
@@ -4,10 +4,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @Builder
 public class RideRequestResponse {
 
-    
+    private Long id;
+    private String requestStatus;
+    private Long rideOfferId;
+    private String requesterEmail;
+    private LocalDateTime requestDate;
 }
