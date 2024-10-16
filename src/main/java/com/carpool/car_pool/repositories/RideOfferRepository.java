69cd9de5ca1e0a792f295c5a4e5f2868522d6ad2
adamp <bjkk32@gmail.com>
2024-10-16 12:09:37 +0000

    Offer repo changes

--- src/main/java/com/carpool/car_pool/repositories/RideOfferRepository.java
+++ src/main/java/com/carpool/car_pool/repositories/RideOfferRepository.java
@@ -3,11 +3,12 @@ package com.carpool.car_pool.repositories;
 import com.carpool.car_pool.repositories.entities.RideOfferEntity;
 import com.carpool.car_pool.repositories.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RideOfferRepository extends JpaRepository<RideOfferEntity, Long> {
+public interface RideOfferRepository extends JpaRepository<RideOfferEntity, Long>, JpaSpecificationExecutor<RideOfferEntity> {
     Optional<RideOfferEntity> findByCreator(UserEntity creator);
 }
