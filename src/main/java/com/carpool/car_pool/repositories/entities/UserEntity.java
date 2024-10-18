be71a3b390f54537080e0ea6303d128dde3f8032
adamp <bjkk32@gmail.com>
2024-10-18 09:50:39 +0000

    Entity import fix

--- src/main/java/com/carpool/car_pool/repositories/entities/UserEntity.java
+++ src/main/java/com/carpool/car_pool/repositories/entities/UserEntity.java
@@ -1,7 +1,18 @@
 package com.carpool.car_pool.repositories.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
