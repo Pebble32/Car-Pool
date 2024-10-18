25e0bfb42a3e37cb003fe0beb0568547ff77d6cc
adamp <bjkk32@gmail.com>
2024-10-18 09:48:17 +0000

    Wildcard import fix

--- src/main/java/com/carpool/car_pool/repositories/common/PageResponse.java
+++ src/main/java/com/carpool/car_pool/repositories/common/PageResponse.java
@@ -1,6 +1,11 @@
 package com.carpool.car_pool.repositories.common;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
