6279fb7e3decf48aa1c92c00d5d627145f139a13
adamp <bjkk32@gmail.com>
2024-10-23 19:06:42 +0000

    File service start

---
+++ src/main/java/com/carpool/car_pool/services/FileStorageService.java
@@ -0,0 +1,10 @@
+package com.carpool.car_pool.services;
+
+import com.carpool.car_pool.repositories.entities.UserEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileStorageService {
+    public void saveProfilePicture(MultipartFile file, UserEntity currentUser) {
+
+    }
+}
