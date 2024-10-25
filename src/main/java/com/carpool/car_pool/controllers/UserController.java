287e16618cea980f38012f527dca18ecd971b9dd
SysMac5 <sigurthor.maggi@gmail.com>
2024-10-25 10:48:35 +0000

    response is paginated

--- src/main/java/com/carpool/car_pool/controllers/UserController.java
+++ src/main/java/com/carpool/car_pool/controllers/UserController.java
@@ -1,16 +1,15 @@
 package com.carpool.car_pool.controllers;
 
 import com.carpool.car_pool.controllers.dtos.UserResponse;
-import com.carpool.car_pool.repositories.entities.UserEntity;
+import com.carpool.car_pool.repositories.common.PageResponse;
 import com.carpool.car_pool.services.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Controller for managing users.
@@ -26,10 +25,16 @@ public class UserController {
     /**
      * Retrieves all users.
      *
-     * @return ResponseEntity containing a list of {@link UserEntity}.
+     * @param page The page number (zero-based).
+     * @param size The size of the page.
+     * @return ResponseEntity containing a {@link PageResponse} of {@link UserResponse}.
      */
-    @GetMapping("/all")
-    public ResponseEntity<List<UserResponse>> getUsers() {
-        return ResponseEntity.ok(userService.findAllUsers());
+    @GetMapping("/all/paginated")
+    public ResponseEntity<PageResponse<UserResponse>> getUsersPaginated(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size
+    ) {
+        PageResponse<UserResponse> response = userService.findAllUsersPaginated(page, size);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/carpool/car_pool/services/UserService.java
+++ src/main/java/com/carpool/car_pool/services/UserService.java
@@ -2,8 +2,14 @@ package com.carpool.car_pool.services;
 
 import com.carpool.car_pool.controllers.dtos.UserResponse;
 import com.carpool.car_pool.repositories.UserRepository;
+import com.carpool.car_pool.repositories.common.PageResponse;
+import com.carpool.car_pool.repositories.entities.UserEntity;
 import com.carpool.car_pool.services.converters.UserConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,14 +24,29 @@ public class UserService {
     private final UserConverter userConverter;
 
     /**
-     * Retrieves all users.
+     * Retrieves paginated list of all users.
      *
-     * @return All users as a list of {@link com.carpool.car_pool.controllers.dtos.UserResponse}.
+     * @param page The page number (zero-based).
+     * @param size The size of the page.
+     * @return A {@link PageResponse} containing the paginated {@link UserResponse}.
      */
-    public List<UserResponse> findAllUsers() {
-        return userRepository.findAll()
-                .stream()
+    public PageResponse<UserResponse> findAllUsersPaginated(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+
+        Page<UserEntity> usersPage = userRepository.findAll(pageable);
+
+        List<UserResponse> users = usersPage.stream()
                 .map(userConverter::entityToDTO)
                 .toList();
+
+        return new PageResponse<>(
+                users,
+                usersPage.getNumber(),
+                usersPage.getSize(),
+                usersPage.getTotalElements(),
+                usersPage.getTotalPages(),
+                usersPage.isFirst(),
+                usersPage.isLast()
+        );
     }
 }
