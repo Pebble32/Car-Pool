5395d2558b2949783196186356ccd71f0134f62b
adamp <bjkk32@gmail.com>
2024-10-28 10:30:44 +0000

    Clean up

--- src/main/java/com/carpool/car_pool/controllers/UserController.java
+++ src/main/java/com/carpool/car_pool/controllers/UserController.java
@@ -4,27 +4,17 @@ import com.carpool.car_pool.controllers.dtos.UserResponse;
 import com.carpool.car_pool.repositories.common.PageResponse;
 import com.carpool.car_pool.repositories.entities.UserEntity;
 import com.carpool.car_pool.services.CurrentUserService;
-import com.carpool.car_pool.services.FileStorageService;
-
 import com.carpool.car_pool.services.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Base64;
 
 
 /**
