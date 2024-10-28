e2b8dd82362f0622a51bde67e8fa1df5d3aca0d6
adamp <bjkk32@gmail.com>
2024-10-28 10:14:29 +0000

    Changes to profile picture

--- src/main/java/com/carpool/car_pool/controllers/UserController.java
+++ src/main/java/com/carpool/car_pool/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.carpool.car_pool.repositories.common.PageResponse;
 import com.carpool.car_pool.services.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
+
 
 /**
  * Controller for managing users.
@@ -25,6 +34,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserController {
 
     private final UserService userService;
+    private final CurrentUserService currentUserService;
 
     /**
      * Retrieves all users.
@@ -54,5 +64,21 @@ public class UserController {
     ) {
         userService.uploadProfilePicture(file);
         return ResponseEntity.accepted().build();
+    }
+
+
+    /**
+     * Get profile-picture of the current user
+     * @return Profile picture as String
+     */
+    @GetMapping("/profile-picture")
+    public ResponseEntity<String> getProfilePicture() {
+        UserEntity currentUser = currentUserService.getCurrentUser();
+        byte[] image = currentUser.getProfilePicture();
+        if (image == null) {
+            return ResponseEntity.notFound().build();
+        }
+        String encodedImage = Base64.getEncoder().encodeToString(image);
+        return ResponseEntity.ok().body(encodedImage);
     }
 }
