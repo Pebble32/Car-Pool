77cbee309687989a15599ffe17e30f997c2cc6cf
adamp <bjkk32@gmail.com>
2024-11-03 13:47:32 +0000

    Web socket config impl

--- src/main/java/com/carpool/car_pool/configurations/WebSocketConfig.java
+++ src/main/java/com/carpool/car_pool/configurations/WebSocketConfig.java
@@ -1,4 +1,24 @@
-package com.carpool.car_pool.configurations;
+package com.carpool.car_pool.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*") // TODO change origin if needed
+                .withSockJS(); // Fallback options for older browsers
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/app");
+
+        config.enableSimpleBroker("/topic");
+    }
 }
