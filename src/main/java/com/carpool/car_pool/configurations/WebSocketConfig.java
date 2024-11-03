97340e2f812670ceb5c838a2615cbcefe381d8a3
adamp <bjkk32@gmail.com>
2024-11-03 13:58:31 +0000

    Handshaking impl

--- src/main/java/com/carpool/car_pool/configurations/WebSocketConfig.java
+++ src/main/java/com/carpool/car_pool/configurations/WebSocketConfig.java
@@ -1,24 +1,34 @@
-package com.carpool.car_pool.config;
+package com.carpool.car_pool.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final UserHandshakeHandler userHandshakeHandler;
+
+    public WebSocketConfig(UserHandshakeHandler userHandshakeHandler) {
+        this.userHandshakeHandler = userHandshakeHandler;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*") // TODO change origin if needed
-                .withSockJS(); // Fallback options for older browsers
+                .setHandshakeHandler(userHandshakeHandler)
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.setApplicationDestinationPrefixes("/app");
-
-        config.enableSimpleBroker("/topic");
+        // Include /user for user-specific messages
+        config.enableSimpleBroker("/topic", "/queue", "/user");
+        config.setUserDestinationPrefix("/user");
     }
 }
