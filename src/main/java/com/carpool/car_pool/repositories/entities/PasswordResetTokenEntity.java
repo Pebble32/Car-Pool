8ffe4ea3c616fefad25df71108d8aa694866b7e1
MatioS <dkm2@hi.is>
2024-11-06 19:17:54 +0000

    new entity to store the password reset tokens with associated users

---
+++ src/main/java/com/carpool/car_pool/repositories/entities/PasswordResetTokenEntity.java
@@ -0,0 +1,44 @@
+package com.carpool.car_pool.repositories.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "password_reset_tokens")
+public class PasswordResetTokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+}
