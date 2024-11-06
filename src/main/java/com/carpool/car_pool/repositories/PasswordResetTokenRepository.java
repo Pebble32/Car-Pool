04d7a8cdec3189db91c16fd0165d2e927cbd702b
MatioS <dkm2@hi.is>
2024-11-06 19:21:12 +0000

    Repository to handle CRUD for PasswordResetTokenEntity

---
+++ src/main/java/com/carpool/car_pool/repositories/PasswordResetTokenRepository.java
@@ -0,0 +1,10 @@
+package com.carpool.car_pool.repositories;
+
+import com.carpool.car_pool.repositories.entities.PasswordResetTokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetTokenEntity, Long> {
+    PasswordResetTokenEntity findByToken(String token);
+}
