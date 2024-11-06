a083a8df451ffd4e5b2ccd3eda399a2db6926c96
MatioS <dkm2@hi.is>
2024-11-06 12:58:11 +0000

    Fixed imports

--- src/main/java/com/carpool/car_pool/controllers/UserController.java
+++ src/main/java/com/carpool/car_pool/controllers/UserController.java
@@ -8,7 +8,13 @@ import com.carpool.car_pool.services.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
