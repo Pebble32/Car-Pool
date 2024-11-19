25f81f8f7a5cc1dc41894fabd4ba5ffcbc9240cd
Adam Postek <111773160+Pebble32@users.noreply.github.com>
2024-11-19 16:59:10 +0000

    Update CorsConfig.java

--- src/main/java/com/carpool/car_pool/configurations/CorsConfig.java
+++ src/main/java/com/carpool/car_pool/configurations/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("https://brave-moss-04e458603.5.azurestaticapps.net") // Specify your frontend origin
+                        .allowedOrigins("https://kind-sea-06ef2fa0f.5.azurestaticapps.net") // Specify your frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
                         .allowedHeaders("*") // Allow all headers or specify as needed
                         .allowCredentials(true); // Allow credentials
